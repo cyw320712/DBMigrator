b20590fb1f0d8ed909b0f7d5c0360c30eb8a72df
Yongwoo Choi <cyw7515@naver.com>
2022-08-21 00:55:59 +0900

    update: spring boot로 다시 시작

---
+++ src/main/java/com/dbmigrater/DBMigrater/DbMigraterApplication.java
@@ -0,0 +1,13 @@
+package com.dbmigrater.DBMigrater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbMigraterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbMigraterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbmigrater/DBMigrater/DbMigraterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbmigrater.DBMigrater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbMigraterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
