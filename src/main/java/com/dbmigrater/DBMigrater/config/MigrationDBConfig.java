600563cf99bb7654297cbf1c4ce208ab01721e2a
Yongwoo Choi <cyw7515@naver.com>
2022-08-21 23:41:43 +0900

    update: 다중 DB 연결 세팅중

---
+++ src/main/java/com/dbmigrater/DBMigrater/config/MigrationDBConfig.java
@@ -0,0 +1,66 @@
+package com.dbmigrater.DBMigrater.config;
+
+import org.hibernate.dialect.PostgreSQL82Dialect;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy;
+import org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "migrationEntityManager",
+        transactionManagerRef = "migrationTransactionManager",
+        basePackages = "com.dbmigrater.DBMigrater.repository.migration"
+)
+@ConfigurationProperties(prefix = "spring.datasource")
+public class MigrationDBConfig {
+    @Bean
+    @Primary
+    public DataSource migrationDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @Primary
+    public PlatformTransactionManager migrationTransactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(migrationEntityManager().getObject());
+
+        return transactionManager;
+    }
+
+    @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean migrationEntityManager() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(migrationDataSource());
+        em.setPackagesToScan("com.dbmigrater.DBMigrater.entity.migration");
+
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(adapter);
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.physical_naming_strategy", SpringPhysicalNamingStrategy.class.getName());
+        properties.put("hibernate.implicit_naming_strategy", SpringImplicitNamingStrategy.class.getName());
+        properties.put("hibernate.dialect", PostgreSQL82Dialect.class.getName());
+
+        em.setJpaPropertyMap(properties);
+
+        return em;
+    }
+}
