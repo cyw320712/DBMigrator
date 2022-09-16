135290dc706f372dc71574195acc392063554a2e
Yongwoo Choi <cyw7515@naver.com>
2022-09-16 23:13:03 +0900

    Delete: 불필요해진 Legacy Repository 삭제

--- src/main/java/com/dbmigrator/DBMigrator/domain/legacy/LegacyExampleRepository.java
+++
@@ -1,6 +0,0 @@
-package com.dbmigrator.DBMigrator.domain.legacy;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LegacyExampleRepository extends MongoRepository<LegacyExample, Long> {
-}
