e8aa40e03700efaa9d614066a9309ad5657b66e5
Yongwoo Choi <cyw7515@naver.com>
2022-09-16 11:42:16 +0900

    Delete: 불필요해진 MigrationRepository 삭제

--- src/main/java/com/dbmigrator/DBMigrator/domain/migration/MigrationExampleRepository.java
+++
@@ -1,6 +0,0 @@
-//package com.dbmigrator.DBMigrator.domain.migration;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface MigrationExampleRepository extends JpaRepository<MigrationExample, Long> {
-//}
